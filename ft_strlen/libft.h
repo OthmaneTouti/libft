f81ad27e60e97591d66857f7af5746b229b98c8a
OthmaneTouti <touti.othmane@gmail.com>
2023-10-16 19:40:59 -0400

    finished ft_strlen

---
+++ ft_strlen/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 13:44:27 by ottouti           #+#    #+#             */
+/*   Updated: 2023/10/16 19:36:29 by ottouti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalnum(char c);
+int	ft_isalpha(char c);
+int	ft_isdigit(int dig);
+int	ft_isascii(char c);
+int	ft_isprint(char c);
+int	ft_strlen(char *str);
+
+#endif
