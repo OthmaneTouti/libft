9334059a6d16aac095809b498e13b814d14b52ce
Othmane Touti <ottouti@c1r4p8.42quebec.com>
2023-10-18 16:07:44 -0400

    finished ft_strlcat, tested, nm

---
+++ ft_strlcat/ft_strlcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 15:24:34 by ottouti           #+#    #+#             */
+/*   Updated: 2023/10/18 16:07:11 by ottouti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	unsigned long	i;
+
+	i = 0;
+	while (dst[i])
+		i++;
+	while (*src && i < dstsize - 1)
+	{
+		dst[i] = *src++;
+		i++;
+	}
+	dst[i] = '\0';
+	return (dstsize - 1 + ft_strlen((char *)src));
+}
