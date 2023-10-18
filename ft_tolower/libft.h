0ea1b8f549ed774950109caef9fbe4fd8ea6b84a
Othmane Touti <ottouti@c1r4p8.42quebec.com>
2023-10-18 16:32:08 -0400

    finished ft_tolower, tested, nm

---
+++ ft_tolower/libft.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 13:44:27 by ottouti           #+#    #+#             */
+/*   Updated: 2023/10/18 16:31:19 by ottouti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+int		ft_isalnum(char c);
+int		ft_isalpha(char c);
+int		ft_isdigit(int dig);
+int		ft_isascii(char c);
+int		ft_isprint(char c);
+int		ft_strlen(char *str);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+void	*ft_memset(void *ptr, int value, size_t len);
+void	ft_bzero(void *ptr, size_t size);
+void	*ft_memcpy(void *dst, const void *src, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+#endif
