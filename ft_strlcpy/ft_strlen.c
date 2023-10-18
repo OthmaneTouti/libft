b586e8f407a31c9ed743620c0d33c4c60e249f6a
Othmane Touti <ottouti@c1r4p8.42quebec.com>
2023-10-18 14:59:59 -0400

    finished ft_strlcat, tested and nm

--- ft_strlcpy/ft_strlen.c
+++ ft_strlcpy/ft_strlen.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/18 11:59:27 by ottouti           #+#    #+#             */
-/*   Updated: 2023/10/18 14:59:03 by ottouti          ###   ########.fr       */
+/*   Created: 2023/10/16 19:28:43 by ottouti           #+#    #+#             */
+/*   Updated: 2023/10/17 14:21:57 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+int	ft_strlen(char *str)
 {
-	unsigned int	i;
+	int	len;
 
-	i = 0;
-	while (src[i] && i < dstsize - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (ft_strlen((char *)src));
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
 }
--- ft_strlcpy/main.c
+++ ft_strlcpy/main.c
@@ -22,7 +22,7 @@ int	main(void)
 		{
 			printf("\033[0;31mTest 1 Failed!\n\33[0m");
 			printf("Destination: %s\nDestination Check: %s\n", dst, dst_check);
-			printf("Source Length: %d\nSource Length Check: %d\n", src_len, src_len_check);
+			printf("Source Length: %zu\nSource Length Check: %zu\n", src_len, src_len_check);
 			test_fail = 1;
 			break ;
 		}
@@ -32,6 +32,41 @@ int	main(void)
 	{
 		printf("\033[0;32mTest 1 passed!\n\033[0m");
 		printf("Destination: %s\nDestination Check: %s\n", dst, dst_check);
-		printf("Source Length: %d\nSource Length Check: %d\n", src_len, src_len_check);
+		printf("Source Length: %zu\nSource Length Check: %zu\n", src_len, src_len_check);
 	}
+	free(dst);
+	free(dst_check);
+
+	//TEST 2
+	char	*dst2 = (char *) malloc(10 * sizeof(char));
+	char	*dst_check2 = (char *) malloc(10 * sizeof(char));
+	char	*src2 = "42 is the answer.";
+	size_t	dstsize2 = 10;
+	test_fail = 0;
+
+	size_t src_len2 = ft_strlcpy(dst2, src2, dstsize2);
+	size_t src_len_check2 = strlcpy(dst_check2, src2, dstsize2);
+
+	i = 0;
+	while (dst2[i])
+	{
+		if (dst2[i] != dst_check2[i])
+		{
+			printf("\033[0;31mTest 2 Failed!\n\33[0m");
+			printf("Destination: %s$\nDestination Check: %s$\n", dst2, dst_check2);
+			printf("%c != %c\n", dst2[i], dst_check2[i]);
+			printf("Source Length: %zu\nSource Length Check: %zu\n", src_len2, src_len_check2);
+			test_fail = 1;
+			break ;
+		}
+		i++;
+	}
+	if (!test_fail)
+	{
+		printf("\033[0;32mTest 2 passed!\n\033[0m");
+		printf("Destination: %s$\nDestination Check: %s$\n", dst2, dst_check2);
+		printf("Source Length: %zu\nSource Length Check: %zu\n", src_len2, src_len_check2);
+	}
+	free(dst2);
+	free(dst_check2);
 }
