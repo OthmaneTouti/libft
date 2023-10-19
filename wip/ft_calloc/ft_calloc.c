32d3a92758892cd85c4ad711699a8e1a59663387
OthmaneTouti <80723817+OthmaneTouti@users.noreply.github.com>
2023-10-19 19:03:10 -0400

    corrected nm for ft_calloc

--- wip/ft_calloc/ft_calloc.c
+++ wip/ft_calloc/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 18:46:18 by ottouti           #+#    #+#             */
-/*   Updated: 2023/10/19 18:50:27 by ottouti          ###   ########.fr       */
+/*   Updated: 2023/10/19 19:02:51 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void *ptr;
-	
+	void	*ptr;
+
 	ptr = (void *) malloc(count * size);
 	ft_bzero(ptr, count * size);
 	return (ptr);
