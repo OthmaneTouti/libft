8863422b0cb807ed8fd18cd95f2d5c4fa64abbb2
OthmaneTouti <80723817+OthmaneTouti@users.noreply.github.com>
2023-10-19 14:22:36 -0400

    finished ft_strnstr, tested, nm ok

--- wip/ft_strnstr/ft_strnstr.c
+++ wip/ft_strnstr/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:01:01 by ottouti           #+#    #+#             */
-/*   Updated: 2023/10/19 14:01:11 by ottouti          ###   ########.fr       */
+/*   Updated: 2023/10/19 14:22:19 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!needle[i])
+		return ((char *)haystack);
+	while (i < len && haystack[i])
+	{
+		while (haystack[i] == needle[j])
+		{
+			i++;
+			j++;
+			if (!needle[j + 1])
+				return ((char *)&haystack[i - j]);
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
 }
