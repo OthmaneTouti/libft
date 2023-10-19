7f5517741c8e9fb2b38953eab6216c6a6dfeb9a2
OthmaneTouti <80723817+OthmaneTouti@users.noreply.github.com>
2023-10-18 20:59:39 -0400

    finished ft_strrchr.c, tested, nm

---
+++ ft_strrchr/libft.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 13:44:27 by ottouti           #+#    #+#             */
+/*   Updated: 2023/10/18 20:51:39 by ottouti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+int		ft_isalnum(char c);
+int		ft_isalpha(char c);
+int		ft_isdigit(int dig);
+int		ft_isascii(char c);
+int		ft_isprint(char c);
+int		ft_strlen(char *str);
+int		ft_toupper(int c);
+void	*ft_memset(void *ptr, int value, size_t len);
+void	ft_bzero(void *ptr, size_t size);
+void	*ft_memcpy(void *dst, const void *src, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+#endif
