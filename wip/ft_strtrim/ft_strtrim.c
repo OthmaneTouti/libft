c5e25ee1e7f3701d6db3cd8488a9606178f258c7
OthmaneTouti <80723817+OthmaneTouti@users.noreply.github.com>
2023-10-21 18:26:45 -0400

    corrected syntax mistake

--- wip/ft_strtrim/ft_strtrim.c
+++ wip/ft_strtrim/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 14:01:27 by ottouti           #+#    #+#             */
-/*   Updated: 2023/10/21 18:19:39 by ottouti          ###   ########.fr       */
+/*   Updated: 2023/10/21 18:26:27 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strtrim(const char *s1, const char *set)
 	len_set = ft_strlen((char *)set);
 	len_s1 = ft_strlen((char *)s1);
 	trimmed_str = (char *) malloc(len_s1 - 2 * len_set);
-	if (trimmed_str!)
+	if (!trimmed_str)
 		return (0);
 	if (!ft_strncmp(s1, set, len_set)
 		&& !ft_strncmp((s1 + len_s1 - len_set), set, len_set))
