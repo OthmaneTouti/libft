22b693b3eb99f86d0ad6f3b4e0aff507ce1e3c1d
OthmaneTouti <80723817+OthmaneTouti@users.noreply.github.com>
2023-10-23 16:06:16 -0400

    ran paco on ft_striteri, corrected mistake

--- wip/ft_striteri/ft_striteri.c
+++ wip/ft_striteri/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:01:06 by ottouti           #+#    #+#             */
-/*   Updated: 2023/10/23 11:32:27 by ottouti          ###   ########.fr       */
+/*   Updated: 2023/10/23 16:05:45 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	while (s[i])
 	{
-		f(i, s);
+		f(i, s + i);
 		i++;
 	}
 }
