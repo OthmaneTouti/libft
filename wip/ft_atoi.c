2c25ac2ad8ba2c597ade1e4e10497a37509aada7
OthmaneTouti <80723817+OthmaneTouti@users.noreply.github.com>
2023-10-23 17:05:27 -0400

    ran paco on ft_atoi, corrected mistake

--- wip/ft_atoi.c
+++ wip/ft_atoi.c
@@ -6,11 +6,20 @@
 /*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:36:41 by ottouti           #+#    #+#             */
-/*   Updated: 2023/10/19 15:27:00 by ottouti          ###   ########.fr       */
+/*   Updated: 2023/10/23 17:02:33 by ottouti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int ft_isspace(char c)
+{
+	if (c == '\t' || c == '\n' ||
+	    c == '\v' || c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	else
+		return (0);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -19,18 +28,24 @@ int	ft_atoi(const char *str)
 
 	sign = 1;
 	num = 0;
-	while (*str < 33 || *str == '+' || *str == '-'
+	while (ft_isspace(*str) || *str == '+' || *str == '-'
 		|| (*str >= '0' && *str <= '9'))
 	{
-		if (*str == '+'
+		if ((*str == '+' || *str =='-')
 			&& !(*(str + 1) >= '0' && *(str + 1) <= '9'))
-			return (num);
-		if (*str == '-' && sign == -1)
 			return (num);
 		else if (*str == '-' && sign == 1)
 			sign = -1;
 		else if (*str >= '0' && *str <= '9')
-			num = num * 10 + (*str - '0');
+		{
+			while (*str && (*str >= '0' && *str <= '9'))
+			{
+				num = num * 10 + (*str - '0');
+				str++;
+			}
+			return (num * sign);
+		}
+		
 		str++;
 	}
 	return (num * sign);
